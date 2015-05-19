74f0e86a58e67df69f69d969801ea7648582d915
Alex Whitman <alex.whitman@durham.ac.uk>
2015-05-19 09:58:57 +0100

    Add jetty installation to vagrant provision

--- vagrant/user-provision.sh
+++ vagrant/user-provision.sh
@@ -3,3 +3,18 @@
 echo "gem: --no-rdoc --no-ri" > ~/.gemrc
 
 gem install bundler rails
+
+cd /opt/sufia
+
+bundle install
+
+rake db:migrate
+
+rake jetty:clean
+rake sufia:jetty:config
+
+# Move fcrepo4-data directory to local file system
+mkdir /tmp/fcrepo4-data
+ln -s /tmp/fcrepo4-data jetty/
+
+rake jetty:start
