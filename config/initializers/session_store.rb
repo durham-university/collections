c0a454db42ae4f85fd6c60bc37c75a5636974f9e
Alex Whitman <alex.whitman@durham.ac.uk>
2015-05-29 14:06:05 +0100

    Store session data in Redis
    Allows for a centralised Redis session store to be used so that
    sessions aren't tied to individual web backends.  This means that
    web backends can be stopped/restarted when needed without interrupting
    a users session.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,14 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_sufia_session'
+config = YAML::load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis_session.yml'))).result)[Rails.env].with_indifferent_access
+
+Rails.application.config.session_store :redis_session_store, {
+  key: '_sufia_session',
+  redis: {
+    db: config[:db] || 1,
+    expire_after: (config[:expire] || 120).minutes,
+    key_prefix: config[:keyprefix] || 'sufia:session:',
+    host: config[:host] || 'localhost',
+    port: config[:port] || 6379
+  }
+}
