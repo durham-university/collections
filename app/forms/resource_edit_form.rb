5069225d2457154ce1adbcf9165247ed1f22f268
Tim Packer <t.s.packer@durham.ac.uk>
2015-05-29 10:19:49 +0100

    Edit fields for complex authors

--- app/forms/resource_edit_form.rb
+++ app/forms/resource_edit_form.rb
@@ -13,6 +13,36 @@ class ResourceEditForm < ResourcePresenter
     end
 
     def self.permitted_authors_params
-      [ :id, :_destroy, :first_name, :last_name ]
+      [ 
+        :id, 
+        :_destroy, 
+        {
+          first_name: [],
+          last_name: []
+        },
+        :orcid
+      ]
+    end
+
+    # Override HydraEditor::Form to treat nested attributes accordingly
+    def initialize_field(key)
+      if reflection = model_class.reflect_on_association(key)
+        raise ArgumentError, "Association ''#{key}'' is not a collection" unless reflection.collection?
+        build_association(key)
+      else
+        super
+      end
+    end
+
+  private
+
+    def build_association(key)
+      association = model.send(key)
+      if association.empty?
+        self[key] = Array(association.build)
+      else
+        association.build
+        self[key] = association
+      end
     end
 end
