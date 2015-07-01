e81a4c789290d1237447b25821276dd4652777ef
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2015-07-01 15:59:02 +0100

    Fix Sufia render_visibility_label helper. A document may be both registered and public in which case public should take precedence.

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -38,4 +38,15 @@ module SufiaHelper
       return ['autocomplete']
     end
   end
+
+
+  def render_visibility_label document
+    if document.public?
+      content_tag :span, t('sufia.visibility.open'), class: "label label-success", title: t('sufia.visibility.open_title_attr')
+    elsif document.registered? 
+      content_tag :span, t('sufia.institution_name'), class: "label label-info", title: t('sufia.institution_name')
+    else
+      content_tag :span, t('sufia.visibility.private'), class: "label label-danger", title: t('sufia.visibility.private_title_attr')
+    end
+  end
 end
