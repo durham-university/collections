0ead81710c86bc0460e5f64b502a7c86e44e2d74
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2015-07-03 11:58:20 +0100

    Small fix for open access pending to make it behave exactly like authenticated.

--- app/controllers/concerns/hydra_durham/visibility_params.rb
+++ app/controllers/concerns/hydra_durham/visibility_params.rb
@@ -5,7 +5,7 @@ module HydraDurham
     def handle_pending_visibility_params(params,resource,metadata_key)
       # NOTE: params usually has not been sanitised, be careful!
       if params[:visibility]=='open-pending'
-        params.delete :visibility
+        params[:visibility]=Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
         params[metadata_key]||=ActionController::Parameters.new()
         params[metadata_key][:request_for_visibility_change]=Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
       elsif params[:visibility]
