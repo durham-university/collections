11e0f0e805d85bd2e1c665ff2cc7af4f39a1c115
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2015-07-03 14:27:52 +0100

    Show only owned collections in the my collections list.

--- app/controllers/my/collections_controller.rb
+++ app/controllers/my/collections_controller.rb
@@ -2,6 +2,10 @@ module My
   class CollectionsController < MyController
 
     self.search_params_logic += [
+      # This is fixed in sufia github. Temporarily copied solution until we
+      # upgrade to new Sufia.
+      :show_only_files_deposited_by_current_user,
+      
       :show_only_collections
     ]
 
