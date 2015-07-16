192ac8424da07cd7d0ba5089d18367138bb3c612
Tim Packer <t.s.packer@durham.ac.uk>
2015-07-16 15:03:26 +0100

    Replace affiliation predicate

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -7,7 +7,7 @@ class Contributor < ActiveFedora::Base
     index.as :stored_searchable
   end
 
-  property :affiliation, predicate: ::RDF::FOAF.currentProject do |index|
+  property :affiliation, predicate: ::RDF::URI.new('http://collections.durham.ac.uk/ns#contributor_affiliation') do |index|
     index.as :stored_searchable
   end
 
