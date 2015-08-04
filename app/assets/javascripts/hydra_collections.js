72c2bd0f761fedd0cb629e7c7a1c4f24c80db110
Olli Lyytinen <olli.lyytinen@durham.ac.uk>
2015-08-04 10:49:14 +0100

    Remember last selected collection and select that in the list when adding files to a collection.

--- app/assets/javascripts/hydra_collections.js
+++ app/assets/javascripts/hydra_collections.js
@@ -1,10 +1,15 @@
 Blacklight.onLoad(function () {
 
   // change the action based which collection is selected
-  $('input.updates-collection').on('click', function() {
+  $('input.updates-collection').on('click', function(event) {
 
       var form = $(this).closest("form");
-      var collection_id = $(".collection-selector:checked")[0].value;
+      var checked = $(".collection-selector:checked");
+      if( checked.length == 0 ){
+        event.preventDefault();
+        return false;
+      }
+      var collection_id = checked[0].value;
       form[0].action = form[0].action.replace("collection_replace_id",collection_id);
       form.append('<input type="hidden" value="add" name="collection[members]"></input>');
 
